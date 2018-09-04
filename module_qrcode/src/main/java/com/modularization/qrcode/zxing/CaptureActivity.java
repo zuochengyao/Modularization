31d34fbbd1e489ce6679b0a3f2f443241fa2f263
zcy_work <zuochengyao@megvii.com>
2018-09-04 15:05:33 +0800

    ViewfinderView 四个角蓝色框

--- module_qrcode/src/main/java/com/modularization/qrcode/zxing/CaptureActivity.java
+++ module_qrcode/src/main/java/com/modularization/qrcode/zxing/CaptureActivity.java
@@ -612,7 +612,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
         ImageView barcodeImageView = (ImageView) findViewById(R.id.barcode_image_view);
         if (barcode == null)
         {
-            barcodeImageView.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.launcher_icon));
+            barcodeImageView.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.launcher_icon));
         }
         else
         {
